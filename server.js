a9836a767265aef80714903f7b19e0120659566c
Andrew Gray <natreve.ag20@gmail.com>
2018-04-29 23:21:34 -0500

    Changed app.js to server.js, moved api calls to routes\api.js

---
+++ server.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+//Middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+//Page routes
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/home.html'));
+app.get('/terms', (req, res) => res.sendFile(__dirname + '/views/terms.html'));//Splash page
+app.get('/register', (req, res) => res.sendFile(__dirname + '/views/register.html'));
+app.get('/preferences', (req, res) => res.sendFile(__dirname + '/views/preferences.html'));
+app.get('/characteristics', (req, res) => res.sendFile(__dirname + '/views/characteristics.html'));
+
+//API Routes
+app.use('/api', require('./routes/api'));//API ROUTES
+app.listen(port, () => console.log(`Listening on port ${port}...`));
